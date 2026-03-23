461e229e3b58bc6c52dc80f0de091e768fecd2fa
MuzaffarTursunov96 <muzaffar.tursunov102196@gmail.com>
2026-03-23 18:02:51 +0500

    sdf

--- backend/bot/handlers/__init__.py
+++ backend/bot/handlers/__init__.py
@@ -1,4 +1,6 @@
 from .start import router as start_router
+from .root_expiry import router as root_expiry_router
 
 def setup_routers(dp):
     dp.include_router(start_router)
+    dp.include_router(root_expiry_router)
